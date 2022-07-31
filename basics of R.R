53d2b33383505094a75f20845b82fd946b3dc396
sravankankatala <109017898+sravankankatala@users.noreply.github.com>
2022-07-31 16:55:00 +0530

    Add files via upload
    Basics of R. Operators, Data frames & slicing the data frames

---
+++ basics of R.R
@@ -0,0 +1,28 @@
+x = 2
+x
+x<- c(10.3, 20L, "Sravan", TRUE)
+typeof(x)
+y<- c(10.3, 20L, TRUE)
+typeof(y)
+
+rep(1, 10)
+rep(c(10,11),20)
+x=c(2,3,5)
+y=c(32,43,43)
+x[2]
+1:30
+20:1
+f=c(1:10,9:1)
+f
+xyz = c(1,5,2)
+xyz1 = c(rep(c(10,11),11),rep(2,10))
+xyz1
+
+
+xyz2 = c(rep(1,10),rep(5,10),rep(2,10))
+xyz2
+xyz3 = c(rep(1,11),rep(5,12),rep(2,13))
+xyz3
+
+32^(1/5)
+round(0.01235,3)
